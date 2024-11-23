1f0beca57911638aeae0cfde81cde693633b379e
BrunoGeraldine <databrunog@gmail.com>
2024-11-23 08:11:37 -0300

    app buiding

--- src/V_c01_ETL_Clean.py
+++ src/V_c01_ETL_Clean.py
@@ -2,6 +2,7 @@ import re
 import requests
 import pandas        as pd
 
+from datetime        import datetime
 from bs4             import BeautifulSoup
 
 list_auto = []
@@ -55,7 +56,7 @@ for link in links_gerados:
      #Seller/Location
     seller_detail = soup.find('div', attrs={'class':re.compile('SellerInfo')})
     seller = seller_detail.find('span', attrs={'class':re.compile('SellerInfo_name')}).get_text().strip()
-    endereco = seller_detail.find('span', attrs={'class':re.compile('SellerInfo_address')}).get_text().strip()
+    address = seller_detail.find('span', attrs={'class':re.compile('SellerInfo_address')}).get_text().strip()
 
     for ads in add:
         auto = soup.find('a', attrs={'class':re.compile('ListItem_title')}).get_text().strip()
@@ -68,31 +69,38 @@ for link in links_gerados:
         description_car = ads.find('div', attrs={'class': re.compile("ListItem_wrapper")}).getText()
         version = ads.find('span', attrs={'class': re.compile("ListItem_version")}).getText()
         
-
-
-
         list_auto.append([id_ads, auto, manufacturer_by, 
                           model, year, km, price, description_car, 
-                          version, seller, endereco])
+                          version, seller, address])
         df = pd.DataFrame(list_auto, columns=["id_ads", "auto", "manufacturer_by", 
                                               "model", "year", "km", "price", "description_car", 
-                                              "version", "seller", "endereco"])
+                                              "version", "seller", "address"]).reset_index(drop=True)
+        #df['scrapy_datetime'] = datetime.now().strftime('%Y-%m-%d')
 
-rows = df.shape[0]
+        # Carregue a tabela já existente, se houver
+        try:
+            tabela_existente = pd.read_csv('../dataset/list_auto.csv', encoding='utf-8', sep=';')
+        except FileNotFoundError:
+            # Se a tabela não existe, crie um DataFrame vazio
+            tabela_existente = pd.DataFrame()
+            # Concatening new DataFrame with exist table
+        df_new = pd.concat([tabela_existente, df], ignore_index=False)
+
+rows = df_new.shape[0]
 print(rows)
-df.to_csv('../dataset/list_auto_paginacao.csv')
+#df.to_csv('../dataset/list_auto_paginacao.csv')
 
 #df_gross = pd.read_csv('../dataset/list_auto_paginacao.csv')
 #
 #df_gross.to_excel('../dataset/list_auto_pag.xlsx', index=None, header=True)
 
 ######## ETL - Cleaning process
-
-df_clean = pd.read_csv('../dataset/list_auto_paginacao.csv', encoding='utf-8', sep=';')
+df_clean = df.copy()
+#df_clean = pd.read_csv('../dataset/list_auto_paginacao.csv', encoding='utf-8', sep=';')
 
 # "Extracting the postal code from the vehicle's country of origin. r'(\d+)
 # Applying Regex for data cleaning."
-df_clean['zip_code'] = df_clean['address'].apply(lambda x: int(re.search('\d+', x).group(0) if pd.notnull( x ) else x ))
+df_clean['zip_code'] = df_clean['address'].apply(lambda x: int(re.search('[0-9]+', x).group(0) if pd.notnull( x ) else x ))
 
 # Country
 df_clean['city'] = df_clean['address'].apply(lambda x: str(re.search('(?<=IT-\d{5}\s)\w+', x).group(0) if pd.notnull( x ) else x ))
@@ -107,11 +115,40 @@ for i in range(len(df_clean)):
 df_clean['year'] = pd.to_datetime(df_clean['year'], format='%m/%Y')
 df_clean['year'] = df_clean['year'].dt.year
 
+df_clean['scrapy_datetime'] = datetime.now().strftime('%Y-%m-%d')
+
 df_final = pd.DataFrame(df_clean[['id_ads', 'manufacturer_by', 'model', 'version', 'km', 'year',
-                                  'price', 'seller', 'address', 'scrapy_datetime', 'zip_code', 'city']])
+                                  'price', 'seller', 'address', 'zip_code', 'city', 'scrapy_datetime']])
 
 
 rows = df_final.shape[0]
 print(rows)
-df_final.to_csv('../dataset/df_cleaned.csv', encoding='utf-8', sep=';')
+#df_final.to_csv('../dataset/df_cleaned.csv', encoding='utf-8', sep=';')
 
+### ---------- ETL - Tratament and modeling Data
+
+#df_01 = pd.read_csv('../dataset/df_cleaned.csv', encoding='utf-8', sep=';')
+df_01 = df_final.copy()
+
+## 01 - Clean columns
+#df_01 = df_01.drop('Unnamed: 0', axis=1)
+
+
+## 02 Creating Non-Sale History Column
+df_02 = df_01.copy()
+
+count_id_ads = df_02['id_ads'].value_counts()
+df_02['count_id'] = df_02['id_ads'].map(count_id_ads)
+
+## 03 - Creating Today's Date Column
+df_03 = df_02.copy()
+df_03['today'] = pd.to_datetime(datetime.now().strftime('%Y/%m/%d'))
+
+## Converting column types
+df_03['scrapy_datetime'] = pd.to_datetime(df_03['scrapy_datetime']).dt.date
+df_03['scrapy_datetime'] = pd.to_datetime(df_03['scrapy_datetime'])
+
+
+## Save the final dataset
+df_final = pd.DataFrame(df_03)
+df_final.to_csv('../dataset/df_final_model.csv', encoding='utf-8', sep=';')
--- src/app.py
+++ src/app.py
@@ -3,14 +3,22 @@ import streamlit      as st
 import plotly.express as px
 
 
+
 st.title("Vehicle Price Analysis")
 
-uploaded_file = st.file_uploader("Choose file type", type=["xlsx"])
 
-if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file, engine="openpyxlsx")
+st.write("### Know Data")
+df = pd.read_csv('../dataset/df_final_model.csv', encoding='utf-8', sep=';')
 
-    st.write("### Know Data")
-    st.dataframe(df)
+df = df.drop('Unnamed: 0', axis=1)
 
+df['model'] = df['model'].apply(lambda x: 'BMW ' + x)
+#st.table(data=df.head())
 
+st.dataframe(df)
+
+st.write('### Average price for model')
+avg_price_model = df.groupby('model')['price'].mean().reset_index()
+
+fig = px.bar(avg_price_model, x='model', y='price', title='Model Average Price')
+st.plotly_chart(fig)
